a080e2bb7e76323030e3846ff6dffcc7bf0d936d
Johnny Mayer and Sara Hamilton <sara@>
2018-03-27 09:49:12 -0700

    create Recipe class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // currentRecipe: string = this.recipe;
+  title = 'Recipe Box';
+
+  firstRecipe = {
+    title: "coffee",
+    ingredients: "coffee grounds, water",
+    directions: "use coffee maker"
+  }
+  recipes: Recipe[] = [
+    new Recipe('coffee', 'coffee grounds, water', 'use coffee maker'),
+    new Recipe('jello', 'jello mix, water', 'read the box'),
+    new Recipe('hard boiled eggs', 'eggs, water', 'boil eggs for 12 minutes'),
+  ]
 }
