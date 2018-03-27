86c5d7c1bce2db05dbb0dd1515103a41c1b21005
Johnny Mayer and Sara Hamilton <johnny@>
2018-03-27 15:32:28 -0700

    add commented out button and form to add a new recipe to the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,17 @@ export class AppComponent {
   currentFocus: string = 'Recipes that use water';
   title = 'Recipe Box';
 
+  recipes: Recipe[] = [
+    new Recipe('coffee', 'coffee grounds, water', 'use coffee maker'),
+    new Recipe('jell-o', 'jello mix, water', 'read the box'),
+    new Recipe('hard boiled eggs', 'eggs, water', 'boil eggs for 12 minutes'),
+    new Recipe('hot dogs', 'hot dogs, water', 'boil them until they fall apart'),
+    new Recipe('hot chocolate', 'cocoa powder, water, milk', 'microwave it'),
+    new Recipe('frozen water', '8-12 cups water', 'boil it, then freeze to save for later'),
+    new Recipe('ramen', 'ramen packet, msg, salt', 'remove plastic, then boil it, duh'),
+    new Recipe('crystal light', 'crystal light packet, water', 'add 4 crystal light packs to 16oz water')
+  ]
+
   showDetails(clickedRecipe) {
     this.detailedRecipe = clickedRecipe;
   }
@@ -26,16 +37,11 @@ export class AppComponent {
     this.selectedRecipe = null;
   }
 
-  recipes: Recipe[] = [
-    new Recipe('coffee', 'coffee grounds, water', 'use coffee maker'),
-    new Recipe('jell-o', 'jello mix, water', 'read the box'),
-    new Recipe('hard boiled eggs', 'eggs, water', 'boil eggs for 12 minutes'),
-    new Recipe('hot dogs', 'hot dogs, water', 'boil them until they fall apart'),
-    new Recipe('hot chocolate', 'cocoa powder, water, milk', 'microwave it'),
-    new Recipe('frozen water', '8-12 cups water', 'boil it, then freeze to save for later'),
-    new Recipe('ramen', 'ramen packet, msg, salt', 'remove plastic, then boil it, duh'),
-    new Recipe('crystal light', 'crystal light packet, water', 'add 4 crystal light packs to 16oz water')
-  ]
+  createRecipe(recipeTitle, ingredients, directions) {
+    let newRecipe = new Recipe(recipeTitle, ingredients, directions);
+    recipes.push(newRecipe);
+  }
+
 
   selectedRecipe: Recipe = null;
   detailedRecipe: Recipe = null;
